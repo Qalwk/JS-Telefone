d40e3543ee499023c7e4e23b72f411a7ba3f5c9d
Artemii <latovgor@gmail.com>
2025-02-07 17:23:35 +0300

    All-functional

---
+++ script/constants.js
@@ -0,0 +1,2 @@
+export const PATTERN_ABC = /^[a-zA-Z]+$/;
+export const PATTERN_NUM = /^[+]?[0-9]+$/;
--- script/main.js
+++ script/main.js
@@ -1,248 +1,323 @@
-const ElInputName = document.querySelector('.action__input-name');
-const ElInputVacancy = document.querySelector('.action__input-vacancy');
-const ElInputNumber = document.querySelector('.action__input-number');
-const BtnAdd = document.querySelector('.action__btn-add');
-const BtnClear = document.querySelector('.action__btn-clear');
-const BtnTitle = document.querySelectorAll('.list__column-el');
-const ListValue = document.querySelectorAll('.value');
-let ListState = 0;
-let myData = JSON.parse(localStorage.getItem('myData')) || [];
+const elInputName = document.querySelector('.action__input-name');
+const elInputVacancy = document.querySelector('.action__input-vacancy');
+const elInputNumber = document.querySelector('.action__input-number');
+const btnAdd = document.querySelector('.action__btn-add');
+const btnClear = document.querySelector('.action__btn-clear');
+const btnTitle = document.querySelectorAll('.list__column-el');
+const listValue = document.querySelectorAll('.value');
+import { PATTERN_ABC, PATTERN_NUM } from './constants.js';
+let contacts = JSON.parse(localStorage.getItem('contacts')) || [];
 
-
-function CheckListValue() {
-    BtnTitle.forEach(el => { SetListValue(el); }); 
+function checkListValue() {
+    btnTitle.forEach(el => { setValue(el); }); 
 }
-
-BtnAdd.addEventListener('click', function() {
-    const ElObj = ElObjectCreate();
-    if (ElObj.Name && ElObj.Vacancy && ElObj.Number) {
-        const NewDiv = ElementDivCreate(ElObj);
-        const List = getListById(ElObj.Name);
-        appendToList(List, NewDiv);
-        CheckListValue();
-        SaveToLocalStorage(ElObj);
-    }
-});
 
 function saveToLS(key, data) {
     const jsonData = JSON.stringify(data);
     localStorage.setItem(key, jsonData);
 }
 
-function SaveToLocalStorage(data) {
-    myData.push(data);
-    saveToLS('myData', myData);
+function addItem(data) {
+    contacts.push(data);
+    saveToLS('contacts', contacts);
 }
 
 function deleteItem(id) {
-    myData = myData.filter(item => item.id !== id);
-    saveToLS('myData', myData);
+    contacts = contacts.filter(item => item.id !== id);
+    saveToLS('contacts', contacts);
 }
 
 function editItem(id, updatedItem) {
-    myData = myData.map(item => item.id === id ? updatedItem : item);
-    saveToLS('myData', myData);
+    contacts = contacts.map(item => item.id === id ? updatedItem : item);
+    saveToLS('contacts', contacts);
 }
 
-function ElObjectCreate() {
-    const ElObject = {};
+function contactObjectCreate() {
+    const contactObject = {};
+    const elInputInfo = {
+        Name: elInputName.value,
+        Vacancy: elInputVacancy.value,
+        Number: elInputNumber.value
+    };
 
-    const isValid = TextValidate(ElInputName.value, ElInputVacancy.value, ElInputNumber.value);
+    const isValid = textValidate(elInputInfo);
 
-    if (isValid) {
-        ElObject.id = Date.now(); 
-        ElObject.Name = ElInputName.value;
-        ElObject.Vacancy = ElInputVacancy.value;
-        ElObject.Number = ElInputNumber.value;
+    if(!isValid) return null;
 
-        ElInputName.value = '';
-        ElInputVacancy.value = '';
-        ElInputNumber.value = '';
-    }
+    contactObject.id = Date.now();
+    contactObject.Name = elInputName.value;
+    contactObject.Vacancy = elInputVacancy.value;
+    contactObject.Number = elInputNumber.value;
 
-    return ElObject;
+    elInputName.value = '';
+    elInputVacancy.value = '';
+    elInputNumber.value = '';
+
+    console.log(contactObject);
+    
+
+    return contactObject;
 }
 
-function TextValidate(Name, Vacancy, Number) {
-    let RightState = true;
+function textValidate(elInputInfo) {
+    let rightState = true;
 
-    const patternABC = /^[a-zA-Z]+$/; 
-    const patternNUM = /^[+]?[0-9]+$/; 
-
-    if (Name === "" || !patternABC.test(Name)) {
-        ElInputName.classList.add('error');
-        RightState = false;
+    if (elInputInfo.Name === "" || !PATTERN_ABC.test(elInputInfo.Name)) {
+        elInputName.classList.add('error');
+        rightState = false;
     } else {
-        ElInputName.classList.remove('error');
+        elInputName.classList.remove('error');
     }
 
-    if (Vacancy === "" || !patternABC.test(Vacancy)) {
-        ElInputVacancy.classList.add('error');
-        RightState = false;
+    if (elInputInfo.Vacancy === "" || !PATTERN_ABC.test(elInputInfo.Vacancy)) {
+        elInputVacancy.classList.add('error');
+        rightState = false;
     } else {
-        ElInputVacancy.classList.remove('error');
+        elInputVacancy.classList.remove('error');
     }
 
-    if (Number === "" || !patternNUM.test(Number)) {
-        ElInputNumber.classList.add('error');
-        RightState = false;
+    if (elInputInfo.Number === "" || !PATTERN_NUM.test(elInputInfo.Number) || elInputInfo.Number.length !== 12 || elInputInfo.Number[0] != "+") {
+        elInputNumber.classList.add('error');
+        rightState = false;
     } else {
-        ElInputNumber.classList.remove('error');
+        elInputNumber.classList.remove('error');
     }
 
-    return RightState;
+    return rightState;
 }
 
-function ElementDivCreate(data) {
+function elementDivCreate(data) {
     if (!data || !data.Name || !data.Vacancy || !data.Number) {
         console.error('Invalid data:', data);
         return null;
     }
 
-    const NewDiv = document.createElement('div');
-    NewDiv.classList.add('element__wrap', 'unvisible');
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('element__wrap', 'unvisible');
 
-    const NewElName = document.createElement('p');
-    const NewElVacancy = document.createElement('p');
-    const NewElNumber = document.createElement('p');
+    const newElements = newDivElem(data)
 
-    NewElName.textContent = `Name: ${data.Name}`;
-    NewElVacancy.textContent = `Vacancy: ${data.Vacancy}`;
-    NewElNumber.textContent = `Number: ${data.Number}`;
-
-    NewDiv.append(NewElName, NewElVacancy, NewElNumber, addBtnDel(data), addBtnEdit(NewElName, NewElVacancy, NewElNumber, data));
-    return NewDiv;
+    newDiv.append(newElements.name, newElements.vacancy, newElements.number, addActionButton('delete', "", data), addActionButton('edit', newElements, data));
+    return newDiv;
 }
 
-function addBtnDel(data) {
-    const BtnDel = document.createElement('button');
-    BtnDel.classList.add('button', 'btn__delete');
-    BtnDel.textContent = 'Del';
+function newDivElem(data) {
+    const newElName = document.createElement('p');
+    const newElVacancy = document.createElement('p');
+    const newElNumber = document.createElement('p');
 
-    BtnDel.addEventListener('click', function() {
-        BtnDel.parentElement.remove();
-        CheckListValue()
-        deleteItem(data.id);
-    });
+    newElName.textContent = `Name: ${data.Name}`;
+    newElVacancy.textContent = `Vacancy: ${data.Vacancy}`;
+    newElNumber.textContent = `Number: ${data.Number}`;
 
-    return BtnDel;
+    const newElements= {
+        name: newElName,
+        vacancy: newElVacancy,
+        number: newElNumber
+    }
+
+    return newElements;
 }
 
-function addBtnEdit(NewElName, NewElVacancy, NewElNumber, data) {
-    const BtnEdit = document.createElement('button');
-    BtnEdit.classList.add('button');
-    BtnEdit.textContent = 'Edit';
+function addActionButton(type, newElements, data) {
 
-    BtnEdit.addEventListener('click', function() {
-        console.log('BtnEdit clicked');
-        const EditWindow = document.createElement('div');
-        EditWindow.classList.add('element__edit');
+    if (!data || typeof data.id === 'undefined') {
+        console.log('Invalid or missing data:', data);
+        return null;
+    }
 
-        const EditWindowTitle = document.createElement('p');
-        const EditWindowName = document.createElement('input');
-        const EditWindowVacancy = document.createElement('input');
-        const EditWindowNum = document.createElement('input');
-        const BtnSave = document.createElement('button');
+    const button = document.createElement('button');
+    const dataId = data.id;
+    console.log('1:', data);
+    
+    if (type === 'edit') {
+        button.classList.add('button');
+        button.textContent = 'Edit';
+    } else if (type === 'delete') {
+        button.classList.add('button', 'btn__delete');
+        button.textContent = 'Del';
+    } else {
+        console.error('Неизвестный тип кнопки');
+        return null;
+    }
 
-        EditWindowTitle.textContent = 'Edit element';
-        EditWindowName.value = NewElName.textContent.replace('Name: ', '');
-        EditWindowVacancy.value = NewElVacancy.textContent.replace('Vacancy: ', '');
-        EditWindowNum.value = NewElNumber.textContent.replace('Number: ', '');
+    button.addEventListener('click', function () {
+        if (type === 'edit') {
+            const editWindow = document.createElement('div');
+            editWindow.classList.add('element__edit');
 
-        EditWindowName.classList.add('element__edit-input');
-        EditWindowVacancy.classList.add('element__edit-input');
-        EditWindowNum.classList.add('element__edit-input');
+            const editWindowTitle = document.createElement('p');
+            editWindowTitle.textContent = 'Edit element';
 
-        BtnSave.classList.add('button');
-        BtnSave.textContent = 'Save';
+            const editWindowName = document.createElement('input');
+            editWindowName.value = newElements.name.textContent.replace('Name: ', '');
+            editWindowName.classList.add('element__edit-input');
 
-        EditWindow.append(EditWindowTitle, EditWindowName, EditWindowVacancy, EditWindowNum, BtnSave);
+            const editWindowVacancy = document.createElement('input');
+            editWindowVacancy.value = newElements.vacancy.textContent.replace('Vacancy: ', '');
+            editWindowVacancy.classList.add('element__edit-input');
 
-        document.body.appendChild(EditWindow);
+            const editWindowNum = document.createElement('input');
+            editWindowNum.value = newElements.number.textContent.replace('Number: ', '');
+            editWindowNum.classList.add('element__edit-input');
 
-        BtnSave.addEventListener('click', function() {
-            NewElName.textContent = `Name: ${EditWindowName.value}`;
-            NewElVacancy.textContent = `Vacancy: ${EditWindowVacancy.value}`;
-            NewElNumber.textContent = `Number: ${EditWindowNum.value}`;
-            EditWindow.classList.remove('element__edit');
+            const btnSave = document.createElement('button');
+            btnSave.classList.add('button');
+            btnSave.textContent = 'Save';
 
-            const updatedData = {
-                id: data.id,
-                Name: EditWindowName.value,
-                Vacancy: EditWindowVacancy.value,
-                Number: EditWindowNum.value
-            };
+            editWindow.append(editWindowTitle, editWindowName, editWindowVacancy, editWindowNum, btnSave);
+            document.body.appendChild(editWindow);
 
-            editItem(data.id, updatedData);
+            btnSave.addEventListener('click', function () {
+
+                const updatedData = {
+                    id: dataId,
+                    Name: editWindowName.value,
+                    Vacancy: editWindowVacancy.value,
+                    Number: editWindowNum.value
+                };
             
+                if (validateCopied(updatedData)) {
+                    alert('This phone number already exists!');
+                    return;
+                }
 
-            document.body.removeChild(EditWindow);
-            location.reload();
-        });
+                if (!textValidate(updatedData)) {
+                    return alert('Invalid data!');
+                }
+            
+                newElements.name.textContent = `Name: ${updatedData.Name}`;
+                newElements.vacancy.textContent = `Vacancy: ${updatedData.Vacancy}`;
+                newElements.number.textContent = `Number: ${updatedData.Number}`;
+            
+                document.body.removeChild(editWindow);
+                console.log('Edit ID:', dataId);
+                editItem(dataId, updatedData);
+                location.reload();
+
+            });
+        } else if (type === 'delete') {
+            button.parentElement.remove();
+            checkListValue();
+            console.log('Deleting item with ID:', data);
+            deleteItem(dataId);
+        }
     });
 
-    return BtnEdit;
+    return button;
 }
 
 function getListById(name) {
-    let ElId = name.charAt(0);
-    return document.getElementById(ElId);
+    let elIdDefault = name.charAt(0);
+    let elId = elIdDefault.toLowerCase();
+    return document.getElementById(elId);
 }
 
 function appendToList(list, el) {
     list.appendChild(el);
 }
 
-BtnClear.addEventListener('click', function() {
-    removeToList();
-});
-
 function removeToList() {
     const elements = document.querySelectorAll(".element__wrap");
     elements.forEach(el => { el.remove(); });
-    CheckListValue();
-    localStorage.removeItem('myData'); 
+    checkListValue();
+    localStorage.removeItem('contacts'); 
 }
 
-function SetListValue(el) {
-    const Value = el.querySelector('.value');
+function setValue(el) {
+    const value = el.querySelector('.value');
     if (el.children.length <= 1) {
-        Value.textContent = null;
+        value.textContent = null;
     } else {
-        Value.textContent = el.children.length - 1;
+        value.textContent = el.children.length - 1;
     }
 }
 
-BtnTitle.forEach(el => {
-    el.firstChild.addEventListener('click', function() {
+btnClear.addEventListener('click', function() {
+    removeToList();
+});
 
-        if (ListState === 0) {
-            const children = el.querySelectorAll(':scope > .element__wrap');
-            children.forEach(child => {
-                child.classList.remove('unvisible');
-            });
-            ListState = 1;
-            SetListValue(el);
-        } else {
-            const children = el.querySelectorAll(':scope > .element__wrap');
-            children.forEach(child => {
+btnTitle.forEach(el => {
+    let isVisible = false;
+
+    el.firstChild.addEventListener('click', function () {
+        const children = el.querySelectorAll(':scope > .element__wrap');
+        children.forEach(child => {
+            if (isVisible) {
                 child.classList.add('unvisible');
-            });
-            ListState = 0;
-            SetListValue(el);
+            } else {
+                child.classList.remove('unvisible'); 
+            }
+        });
+
+        isVisible = !isVisible;
+
+        setValue(el);
+    });
+});
+
+btnAdd.addEventListener('click', function() {
+    const elObj = contactObjectCreate();
+    console.log(elObj);
+
+    if (elObj && elObj.Name && elObj.Vacancy && elObj.Number) {
+        if (validateCopied(elObj)) {
+            return alert('Dublicate!');
         }
-        console.log('ListState after:', ListState);
+        const newDiv = elementDivCreate(elObj);
+        const list = getListById(elObj.Name);
+        appendToList(list, newDiv);
+        checkListValue();
+        addItem(elObj);
+    }
+});
+
+function validateCopied(elObj) {
+    const allEl = document.querySelectorAll(".element__wrap");
+
+    for (const el of allEl) {
+        // const name = el.querySelector('p:nth-child(1)').textContent.replace('Name: ', '').trim();
+        // const vacancy = el.querySelector('p:nth-child(2)').textContent.replace('Vacancy: ', '').trim();
+        const number = el.querySelector('p:nth-child(3)').textContent.replace('Number: ', '').trim();
+
+        if (
+            // name === elObj.Name ||
+            // vacancy === elObj.Vacancy ||
+            number === elObj.Number
+        ) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+btnTitle.forEach(el => {
+    let isVisible = false;
+
+    el.firstChild.addEventListener('click', function () {
+        const children = el.querySelectorAll(':scope > .element__wrap');
+        children.forEach(child => {
+            if (isVisible) {
+                child.classList.add('unvisible');
+            } else {
+                child.classList.remove('unvisible'); 
+            }
+        });
+
+        isVisible = !isVisible;
+
+        setValue(el);
     });
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-    myData.forEach(data => {
-        const NewDiv = ElementDivCreate(data);
-        if (NewDiv) {
-            const List = getListById(data.Name);
-            appendToList(List, NewDiv);
+    contacts.forEach(data => {
+        const newDiv = elementDivCreate(data);
+        if (newDiv) {
+            const list = getListById(data.Name);
+            appendToList(list, newDiv);
         }
     });
-    CheckListValue();
-    console.log('Data loaded from localStorage:', myData);
+    checkListValue();
 })
